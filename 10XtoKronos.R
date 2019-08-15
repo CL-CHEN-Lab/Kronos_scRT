d3089ede231c0f6e22626aa4307777fbf2761653
Stefano Gnan <stefanognan@gmail.com>
2019-08-15 12:45:32 +0200

    Rishape 10X genomics as Kronos CNV

---
+++ 10XtoKronos.R
@@ -0,0 +1,83 @@
+if (!suppressPackageStartupMessages(require(optparse, quietly = TRUE))) {
+    install.packages("optparse", quiet = T)
+    suppressPackageStartupMessages(library(optparse, quietly = TRUE))
+}
+
+options(stringsAsFactors = FALSE)
+options(warn = 1)
+
+option_list = list(
+    make_option(
+        c("-F", "--file"),
+        type = "character",
+        default = NULL,
+        help = "Per cell stat file , if multiple files are provided they have to be separated by a comma",
+        metavar = "character"
+    ),
+    make_option(
+        c("-T", "--tracks"),
+        type = "character",
+        default = NULL,
+        help = "Tracks file,  if multiple files are provided they have to be separated by a comma",
+        metavar = "character"
+    ),
+    make_option(
+        c("-o", "--out"),
+        type = "character",
+        default = "output",
+        help = "output directory [default= %default]",
+        metavar = "character"
+    )
+)
+
+#recover inputs
+opt = parse_args(object = OptionParser(option_list = option_list))
+if (!suppressPackageStartupMessages(require(tidyverse, quietly = TRUE))) {
+    install.packages("tidyverse", quiet = T)
+    suppressPackageStartupMessages(library(tidyverse, quietly = TRUE))
+}
+
+#create directory
+
+if (str_extract(opt$out,'.$')!='/'){
+    opt$out=paste0(opt$out,'/')
+}
+
+system(paste0('mkdir -p ./', opt$out))
+
+#load files
+opt$file = str_split(opt$file, ',')[[1]]
+
+opt$tracks = str_split(opt$tracks, ',')[[1]]
+if ('file' %in% names(opt)) {
+    for (file in opt$file) {
+        read_csv(file) %>%
+            select(cell_id,
+                   normalized_dimapd,
+                   mean_ploidy,
+                   ploidy_confidence,
+                   is_high_dimapd,
+                   is_noisy) %>%
+            `colnames<-`(c(
+                'Cell',
+                'normalized_dimapd',
+                'mean_ploidy',
+                'ploidy_confidence',
+                'is_high_dimapd',
+                'is_noisy'
+            )) %>%
+            write_csv(paste0(opt$out, '/Kronos_format_', basename(file)))
+    }
+}
+if ('tracks' %in% names(opt)) {
+    for (tracks in opt$tracks) {
+        read_tsv(tracks, skip = 2) %>%
+            select(id, `#chrom`, start, end, copy_number) %>%
+            `colnames<-`(c('Cell', 'chr', 'start', 'end', 'copy_number')) %>%
+            write_tsv(paste0(opt$out, '/Kronos_format_', basename(tracks)))
+    }
+}
+
+if (!('tracks' %in% opt & 'file' %in% opt )){
+    stop('No input')
+}
