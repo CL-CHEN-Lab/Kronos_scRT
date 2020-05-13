23041a20e944d0f2fdea4f0812093e7cfabc722a
Stefano Gnan <stefanognan@gmail.com>
2020-05-13 14:39:58 +0200

    Add Rpks Installer

---
+++ modules/Kronos_scRT_Rpackages_installer.R
@@ -0,0 +1,13 @@
+#!/usr/local/bin/Rscript --slave
+
+#install all the required R packages
+
+install.packages(pkgs =c('Cairo','doSNOW','foreach','gplots','LaplacesDemon','MASS','matrixStats','optparse',
+                   'RColorBrewer','scales','tidyverse'), dependencies = T , repos = "http://cran.us.r-project.org")
+
+
+if (!requireNamespace("BiocManager", quietly = TRUE))
+    install.packages("BiocManager")
+
+BiocManager::install(pkgs = c("DNAcopy","Biostrings",'GenomicRanges','Rbowtie2','Rsamtools'))
+
