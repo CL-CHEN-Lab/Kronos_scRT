9baf49e647146dd3f49d9cd9e6f3ea48aae78c4d
Joseph Josephides <49646747+josephides@users.noreply.github.com>
2020-11-16 10:47:35 +0100

    Update Kronos_fastqtoBAM.R

--- modules/Kronos_fastqtoBAM.R
+++ modules/Kronos_fastqtoBAM.R
@@ -471,6 +471,10 @@ if(!opt$keep_intermediate_files){
 
 stopCluster(cl)
 
-tmp%>%write_tsv(paste(opt$output_dir,'Single_cells_reads_info.tsv'))
+if (file.exists(paste(opt$output_dir,'Single_cells_reads_info.tsv'))){
+    tmp%>%write_tsv(paste(opt$output_dir,'Single_cells_reads_info.tsv'), append = T)
+}else{
+    tmp%>%write_tsv(paste(opt$output_dir,'Single_cells_reads_info.tsv')) 
+}
 
 print('done')
